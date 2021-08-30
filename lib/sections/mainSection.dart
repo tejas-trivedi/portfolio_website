3c0bdce692787143d9d9ebac4e743eb5715bc54b
tejas-trivedi <500077185@stu.upes.ac.in>
2021-08-30 21:25:17 +0530

    desktop view resume update

--- lib/sections/mainSection.dart
+++ lib/sections/mainSection.dart
@@ -173,7 +173,7 @@ class _MainPageState extends State<MainPage> {
                   side: BorderSide(color: kPrimaryColor)),
               onPressed: () {
                 html.window.open(
-                    'https://drive.google.com/file/d/15ibZ7_6JBbPHsy4i9pOGvflJ86fV3k_e/view?usp=sharing',
+                    'https://drive.google.com/file/d/1-KQmV-MHM3fU3sk1aKn50YbwoBXLHvYe/view?usp=sharing',
                     "pdf");
               },
               child: Text(
